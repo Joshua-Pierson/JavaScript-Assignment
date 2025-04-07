050995d36a526e9868b17da61154e056d6d18737
Joshua Pierson <pierson.joshua726@gmail.com>
2025-04-07 18:28:44 -0500

    update

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-let student1 ={
-    name: "John Doe",
+let students =[
+    {name: "John Doe",
     age: 20,
-    isEnrolled: true
-};
-let student2 ={
-    name: "James Smith",
+    isEnrolled: true},
+
+    {name: "James Smith",
     age: 22,
-    isEnrolled: true
-};
-let student3 ={
-    name: "Josh Brown",
+    isEnrolled: true},
+
+    {name: "Josh Brown",
     age: 19,
-    isEnrolled: false
-};
+    isEnrolled: false},
+]
+console.log(students);  // this will print the array of students.
 
-let students = [student1, student2, student3];
-
-students.forEach(student => {
-    console.log(`Name: ${student.name}, Age: ${student.age}, Enrolled: ${student.isEnrolled}`);// This will print each student's details
-});
-console.log(student1.name);// this will print the name of student 1.
-console.log(student2.age);// this will print the age of student 2.
-console.log(student3.isEnrolled); //  this is whether student 3 is enrolled or not.
-console.log(typeof student1.name, typeof student1.age, typeof student1.isEnrolled); // this will print the type of each property in student1 object.
+console.log(students[0].name);  // this will print the name of the first student in array.
+console.log(students[1].age);    // this will print the age of second student in array.
+console.log(students[2].isEnrolled); //  this is whether the third student in the array is enrolled or not.
+console.log(typeof students[0].name);   // this will print the type of the name of first student in array.
+console.log(typeof students[1].age);    // this will print the type of the age of second student in array.
+console.log(typeof students[2].isEnrolled); // this will print the type of the isEnrolled of third student in array.
